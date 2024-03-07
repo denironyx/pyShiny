dbd5c32328f00339de048dea3f03fc11b6c0af4b
dirorere <dirorere@tripadvisor.com>
2024-03-07 20:43:29 +0000

    layouts components

--- layouts/app.py
+++ layouts/app.py
@@ -2,11 +2,22 @@ import plotly.express as px
 from shiny.express import input, render, ui
 from shinywidgets import render_widget
 
-ui.page_opts(title="Sidebar layout", fillable=True)
+ui.page_opts(title="Multi-page layout", fillable=True)
 
 with ui.sidebar():
     ui.input_select("var", "Select variable", choices=["total_bill", "tip"])
 
-@render_widget
-def hist():
-    return px.histogram(px.data.tips(), input.var())
+
+with ui.nav_panel("Plot"):
+    @render_widget
+    def hist():
+        return px.histogram(px.data.tips(), input.var())
+
+with ui.nav_panel("Table"):
+    @render.data_frame
+    def table():
+        return px.data.tips()    
+
+# @render_widget
+# def hist():
+#     return px.histogram(px.data.tips(), input.var())
