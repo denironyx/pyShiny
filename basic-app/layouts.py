5fc0022ba128b2f058533cb207d6a46a365841ed
dirorere <dirorere@tripadvisor.com>
2024-03-07 20:39:19 +0000

    layouts components

--- basic-app/layouts.py
+++ basic-app/layouts.py
@@ -1,1 +1,14 @@
 from shiny.express import input, ui
+from shinywidgets import render_plotly
+
+ui.input_selectize(
+    "var", "Select variable",
+    choices=["bill_length_mm", "body_mass_g"]
+)
+
+@render_plotly
+def hist():
+    import plotly.express as px
+    from palmerpenguins import load_penguins
+    df = load_penguins()
+    return px.histogram(df, x=input.var())
