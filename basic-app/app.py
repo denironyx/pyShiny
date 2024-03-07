80ec160b9ce69c2894f790f23bfc0a2d276aa525
dirorere <dirorere@tripadvisor.com>
2024-03-07 15:24:36 +0000

    app development

--- basic-app/app.py
+++ basic-app/app.py
@@ -1,18 +1,35 @@
 from shiny.express import input, render, ui
-from shinywidgets import render_plotly
+from shinywidgets import render_plotly, render_altair
 
 ui.page_opts(title="Penguins dashboard", fillable=True)
 
-with ui.sidebar():
-    ui.input_selectize(
-        "var", "Select variable",
-        ["bill_length_mm", "bill_depth_mm", "flipper_length_mm", "body_mass_g", "year"]
-    )
-    ui.input_numeric("bins", "Number of bins", 30)
 
-with ui.card(full_screen=True):
-    @render_plotly
-    def hist():
-        import plotly.express as px
-        from palmerpenguins import load_penguins
-        return px.histogram(load_penguins(), x=input.var(), nbins=input.bins())
+ui.input_selectize(
+    "var", "Select variable",
+    choices=['bill_length_mm', 'body_mass_g']
+)
+
+@render.data_frame
+def head():
+    from palmerpenguins import load_penguins
+    df = load_penguins()
+    return df[["species",input.var()]]
+
+@render_altair
+def hist():
+    import altair as alt
+    from
+
+# with ui.sidebar():
+#     ui.input_selectize(
+#         "var", "Select variable",
+#         ["bill_length_mm", "bill_depth_mm", "flipper_length_mm", "body_mass_g", "year"]
+#     )
+#     ui.input_numeric("bins", "Number of bins", 30)
+
+# with ui.card(full_screen=True):
+#     @render_plotly
+#     def hist():
+#         import plotly.express as px
+#         from palmerpenguins import load_penguins
+#         return px.histogram(load_penguins(), x=input.var(), nbins=input.bins())
