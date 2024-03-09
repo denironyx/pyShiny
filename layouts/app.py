de3d3c427d230130c1548627162a0e3984c83fbe
Dennis Irorere <35724017+denironyx@users.noreply.github.com>
2024-03-09 21:43:57 +0000

    multi page example

--- layouts/app.py
+++ layouts/app.py
@@ -2,21 +2,27 @@ import plotly.express as px
 from shiny.express import input, render, ui
 from shinywidgets import render_widget
 
-ui.page_opts(title="Multi-page layout", fillable=True)
+ui.page_opts(title="Multi-page example", fillable=True)
 
 with ui.sidebar():
     ui.input_select("var", "Select variable", choices=["total_bill", "tip"])
 
+with ui.navset_card_underline(title="Penguins"):
+    with ui.nav_panel("Plot"):
+        @render_widget
+        def hist():
+            return px.histogram(px.data.tips(), input.var())
+        
 
-with ui.nav_panel("Plot"):
-    @render_widget
-    def hist():
-        return px.histogram(px.data.tips(), input.var())
+# with ui.nav_panel("Plot"):
+#     @render_widget
+#     def hist():
+#         return px.histogram(px.data.tips(), input.var())
 
-with ui.nav_panel("Table"):
-    @render.data_frame
-    def table():
-        return px.data.tips()    
+    with ui.nav_panel("Table"):
+        @render.data_frame
+        def table():
+            return px.data.tips()    
 
 # @render_widget
 # def hist():
