514f8840301ec0060d2180eaf4e5c4f7bb92c769
Dennis Irorere <35724017+denironyx@users.noreply.github.com>
2024-03-09 21:55:38 +0000

    layout dashboard

--- layouts/app.py
+++ layouts/app.py
@@ -4,6 +4,8 @@ from shinywidgets import render_widget
 
 ui.page_opts(title="Multi-page example", fillable=True)
 
+tips = px.data.tips()
+
 with ui.sidebar():
     ui.input_select("var", "Select variable", choices=["total_bill", "tip"])
 
@@ -19,10 +21,14 @@ with ui.navset_card_underline(title="Penguins"):
 #     def hist():
 #         return px.histogram(px.data.tips(), input.var())
 
+    # with ui.nav_panel("Table"):
+    #     @render.table
+    #     def table():
+    #         return px.data.tips() 
     with ui.nav_panel("Table"):
         @render.data_frame
         def table():
-            return px.data.tips()    
+            return tips[[input.var()]]
 
 # @render_widget
 # def hist():
